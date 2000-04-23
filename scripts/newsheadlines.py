a8edd7e15dc603d39f2004a4e493bc96f2607925
Joakim <joakim@src.gnome.org>
2000-04-23 12:51:20 +0000

    Tweaks and whatnot on the newsheadlines.py script.

--- scripts/newsheadlines.py
+++ scripts/newsheadlines.py
@@ -1,5 +1,13 @@
 #! /usr/bin/python
 
+# This script grabs the headlines from news.gnome.org in RDF format, and then
+# outputs them as a little HTML table on stdout, suitable for including in
+# the frontpage of the new site. It requires the PyXML package, which is
+# available at http://www.python.org/sigs/xml-sig/files/PyXML-0.5.3.tar.gz
+
+# In the unlikely case that anyone is interested, this is Copyright 2000
+# Joakim Ziegler <joakim@helixcode.com>, and is under the GPL.
+
 import urllib
 import time
 import re
@@ -9,15 +17,15 @@ from xml.sax import saxexts
 
 class PrintHeadline(saxlib.HandlerBase):
 
-    def __init__ (self):
+    def __init__ (self, number):
 	self.inItem = 0
 	self.inTitle = 0
 	self.inLink = 0
-	self.itemTitle = ''
-	self.itemLink = ''
+	self.headlineLimit = number
+	self.headlineCount = 0
 
     def startElement(self, name, attrs):
-
+	if (self.headlineCount >= self.headlineLimit): return
 	if name == 'item':
 	    print "<tr><td colspan='2'><img src='/images/dot.gif' height='6' alt=''></td></tr><tr valign='top'><td width='28' align='center'><img src='/images/news-bullet.png' alt=''></td><td><img src='/images/dot.gif' width='1' height='3' alt=''><br clear='all'>"
 	    self.inItem = 1
@@ -30,13 +38,14 @@ class PrintHeadline(saxlib.HandlerBase):
 
 
     def endElement(self, name):
-
+	if (self.headlineCount >= self.headlineLimit): return
 	if name == 'item':
 	    self.timestamp = re.split("/", self.itemLink)[-2]
 	    self.timetuple = time.gmtime(string.atoi(self.timestamp))
 	    self.timestring = time.strftime("%a, %b %d %Y", self.timetuple)
-	    print "<b><a href='" + self.itemLink + "'>" + self.itemTitle + "</a></b> <font size='-2'>[" + self.timestring + "]</font></td></tr>"
+	    print "<b><a href='" + self.itemLink + "'>" + self.itemTitle + "</a></b> <font size='-2'><nobreak>[" + self.timestring + "]</nobreak></font></td></tr>"
 	    self.inItem = 0
+	    self.headlineCount = self.headlineCount + 1
 	if name == 'title':
 	    self.inTitle = 0
 	if name == 'link':
@@ -44,6 +53,7 @@ class PrintHeadline(saxlib.HandlerBase):
 
 
     def characters (self, ch, start, length):
+	if (self.headlineCount >= self.headlineLimit): return
 	if (self.inItem & self.inTitle):
 	    self.itemTitle = self.itemTitle + ch[start:start+length]
 	if (self.inItem & self.inLink):
@@ -57,7 +67,7 @@ feedfile = urllib.urlopen("http://news.gnome.org/gnome-news/rdf")
 
 if __name__ == '__main__':
     parser = saxexts.make_parser()
-    dh = PrintHeadline()
+    dh = PrintHeadline(5)
     parser.setDocumentHandler(dh)
     parser.parseFile(feedfile)
     parser.close()
