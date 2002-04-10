cb370aac1ddc58948789691678c4b0de92ba1892
Steve Fox <drfickle@uberh4x0r.org>
2002-04-10 18:29:35 +0000

    www.gnome.org/account/first.php
    2002-04-10 Steve Fox <drfickle@uberh4x0r.org>
    
        * www.gnome.org/account/first.php
    
        Remove silly text left from SourceForge code

--- www.gnome.org/account/first.php
+++ www.gnome.org/account/first.php
@@ -14,15 +14,6 @@ site_user_header(array(title=>"Welcome to Gnome.org"));
 
 <P>You are now a registered user on Gnome.org, the home of the GNOME Desktop Environment.
 
-<P>As a registered user, you can participate fully in the activities
-on the site.
-You may now post messages to the project message forums, sign on as a project developer, 
-or even start your own project.
-
-<P>You should take some time to read through the
-<A href="/docs/site/"><b>Site Documentation</b></A> so that you may take
-full advantage of Gnome.org.
-
 <P>Enjoy the site, and please provide us with feedback on ways
 that we can improve Gnome.org.
 
