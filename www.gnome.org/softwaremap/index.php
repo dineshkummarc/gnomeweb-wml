2884c9dd830cabc8c86aeb158552789448dce811
Steve Fox <drfickle@uberh4x0r.org>
2002-04-21 19:16:01 +0000

    www.gnome.org/softwaremap/index.php www.gnome.org/softwaremap/list
    2002-04-21 Steve Fox <drfickle@uberh4x0r.org>
    
        * www.gnome.org/softwaremap/index.php
        * www.gnome.org/softwaremap/list
        * www.gnome.org/softwaremap/search.php
    
        Add a search box to the Software Map

--- www.gnome.org/softwaremap/index.php
+++ www.gnome.org/softwaremap/index.php
@@ -15,10 +15,27 @@ desktop:</p>
 <a href="latest.php">View most recently changed entries</a><br>
 </p>
 
+<br>
+
+<p>
+<form method="post" action="search.php">
+Search for <select name="type_of_search">
+<option value="soft">software
+<option value="people">people
+</select>
+<input type="text" maxlength="128" size="20" name="words">
+<input type="submit" name="Search" value="Search">
+</form>
+</p>
+
+<br>
+
 <p>
 <a href="<?php print $GLOBALS['sys_http_domain']."/account/login.php"; ?>">Log into the software map</a><br>
 <a href="<?php print $GLOBALS['sys_http_domain']."/register/"; ?>">Add a new project to the software map</a><br>
 </p>
+
+<br>
 
 <p>Contact <a href="mailto:webmaster@gnome.org">webmaster@gnome.org</a> with questions.</p>
 
