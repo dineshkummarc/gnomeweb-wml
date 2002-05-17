655714cf690148842e46efd36dfea603913c299c
Steve Fox <drfickle@uberh4x0r.org>
2002-05-17 06:01:35 +0000

    www.gnome.org/friends/*
    2002-05-17 Steve Fox <drfickle@uberh4x0r.org>
    
        * www.gnome.org/friends/*
    
        Add first draft of Friends of GNOME site

---
+++ www.gnome.org/friends/browse.php
@@ -0,0 +1,26 @@
+<?php
+require "pre.php";
+$HTML->header(array(title=>"Friends of GNOME", banner=>"friends"));
+?>
+
+<h2>Become a Friend of GNOME</h2>
+
+<table>
+<tr align="left"><th>Item Name</th><th>Amount/Price</th><th></th></tr>
+<tr><td>Friend</td><td>up to $24.99</td><td><a href="order.php?id=101">Purchase</a></td></tr>
+<tr><td>Associate</td><td>$25.00 to $49.99</td><td><a href="order.php?id=102">Purchase</a></td></tr>
+<tr><td>Benefactor</td><td>$50.00 to $249.99</td><td><a href="order.php?id=103">Purchase</a></td></tr>
+<tr><td>Sponsor</td><td>$250.00 to $999.99</td><td><a href="order.php?id=104">Purchase</a></td></tr>
+<tr><td>Patron</td><td>$1,000.00 to $4999.99</td><td><a href="order.php?id=105">Purchase</a></td></tr>
+<tr><td>Founder</td><td>$5,000 or above</td><td><a href="order.php?id=106">Purchase</a></td></tr>
+<tr><td colspan="3"><br/>Your Friend of GNOME benefits will be effective for one year.<br/><br/></td></tr>
+<tr><td>GNOME t-shirt</td><td>$20</td><td><a href="showitem.php?id=201">View Details</a></td></tr>
+<tr><td>GNOME polo</td><td>?</td><td><a href="showitem.php?id=202">View Details</a></td></tr>
+<tr><td>GNOME coffee mug</td><td>?</td><td><a href="showitem.php?id=203">View Details</a></td></tr>
+<tr><td>GNOME mouse pad</td><td>?</td><td><a href="showitem.php?id=204">View Details</a></td></tr>
+</table>
+
+
+<?php
+$HTML->footer();
+?>
