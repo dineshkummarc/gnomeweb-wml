d02d438885063170d1ab70bcf65218505f07cffd
Nat Friedman <nat@src.gnome.org>
2002-07-13 18:34:21 +0000

    Let the fun begin.

---
+++ www.gnome.org/summit/schedule.php
@@ -0,0 +1,251 @@
+<html>
+<head><title>
+2002 Boston GNOME Summit Schedule</title>
+<style>
+td.nav  {
+        height: 16px;
+        }
+td.cell {
+	background-color: #eeeeee;
+	text-align: center;
+        }
+td.white {
+    background-color: #eeeeee;
+	color: #eeeeee;
+    text-align: top;
+	width: 200px;
+	height: 200px;
+        }
+
+td.title {
+        background-color: #000000;
+        color: #ffffff;
+	text-align: center;
+        height: 25px;
+	font-weight: bold;
+        }
+td.bold {
+        font-weight: bold;
+	font-size: 11px;
+	}
+
+  </style>
+</head>
+<body text="#000000" bgcolor="#FFFFFF" link="#990000" vlink="#990000" alink="#990000">
+
+  <center>
+  <!-- Table for the black border. -->
+  <table border="0" cellspacing="0" cellpadding="0" width="790">
+    <tr><td bgcolor="#000000" colspan=3>
+        <!-- Table for the content image. -->
+        <table border="0" cellspacing="4" cellpadding="4" width="100%">
+          <tr><td bgcolor="#ffffff" align="left">
+
+    	  <!-- The image -->
+    	  <img src="boston-summit.jpg" width=800 height=353 alt="2002 Boston GNOME Summit" border=0>
+
+    	  <!-- The text -->
+          <h1>Summit Schedule</h1>
+
+
+<h2>Thursday, July 18th</h2>
+<table cellpadding=5 cols=3 width="700" text="#ffffff" align=center>
+<tr bgcolor="#000000" align=center>
+    <td width="16%" bgcolor="white"><font color="#ffffff"></font></td>
+    <td width="16%"><font color="#ffffff">Room 33-419</font></td>
+    <td width="16%"><font color="#ffffff">Room 33-418</font></td>
+    <td width="16%"><font color="#ffffff">Room 33-422</font></td>
+    <td width="16%"><font color="#ffffff">Room 1-135</font></td>
+    <td width="16%"><font color="#ffffff">Room 1-136</font></td>
+</tr>
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>9:00 AM</td>
+    <td>Registration & coffee</td> <!-- 33-419 -->
+    <td bgcolor="#ffffff" colspan=4></td> <!-- 33-418 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>10:00 AM</td>
+    <td>Opening address, Nat Friedman</td> <!-- 33-419 -->
+    <td colspan=4 bgcolor="#ffffff"></td> <!-- 33-418 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>11:00 AM</td>
+    <td rowspan=2> Hacking, etc.</td> <!-- 33-419 -->
+    <td rowspan=2>"GNOME Release Process: 2.0 post-mortem and looking ahead," Jeff
+      Waugh.</td> <!-- 33-418 -->
+    <td></td> <!-- 33-422 -->
+    <td></td> <!--  1-135 -->
+    <td rowspan=2>&quot;Extending Nautilus through Views and Scripts,&quot; Dave Camp</td> <!--  1-136 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>12:00 PM</td>
+    <td></td> <!-- 33-422 -->
+    <td></td> <!--  1-135 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>1:00 PM</td>
+    <td colspan=5 align=center bgcolor="#fc87de">Lunch</td>
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>2:00 PM</td>
+    <td rowspan=5> Hacking, etc.</td> <!-- 33-419 -->
+    <td rowspan=2>&quot;Setting your computer's time and other 'sysadmin' tasks,&quot; Chema Celorio</td> <!-- 33-418 -->
+    <td></td> <!-- 33-422 -->
+    <td rowspan=2>&quot;GNOME Project Infrastructure,&quot; Jeff Waugh.</td> <!--  1-135 -->
+    <td></td> <!--  1-136 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>3:00 PM</td>
+    <td></td> <!--  1-135 -->
+    <td></td> <!--  1-136 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>4:00 PM</td>
+    <td rowspan=2>&quot;Building a GNOME Office suite for users,&quot; Martin Sevior.</td>
+    <td></td> <!-- 33-422 -->
+    <td></td> <!--  1-135 -->
+    <td></td> <!--  1-136 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>5:00 PM</td>
+    <td></td> <!-- 33-422 -->
+    <td></td> <!--  1-135 -->
+    <td></td> <!--  1-136 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>6:00 PM</td>
+    <td></td> <!-- 33-418 -->
+    <td></td> <!-- 33-422 -->
+    <td></td> <!--  1-135 -->
+    <td></td> <!--  1-136 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>8:300 PM - 10:30 PM</td>
+    <td colspan=5 align=center>Dinner at Fire + Ice</td>
+</table>
+
+<h2>Friday, July 19th</h2>
+<table cellpadding=5 cols=3 width="700" text="#ffffff" align=center>
+<tr bgcolor="#000000" align=center>
+    <td width="16%" bgcolor="white"><font color="#ffffff"></font></td>
+    <td width="16%"><font color="#ffffff">Room 33-419</font></td>
+    <td width="16%"><font color="#ffffff">Room 33-418</font></td>
+    <td width="16%"><font color="#ffffff">Room 33-422</font></td>
+    <td width="16%"><font color="#ffffff">Room 1-135</font></td>
+    <td width="16%"><font color="#ffffff">Room 1-136</font></td>
+</tr>
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>10:00 AM</td>
+    <td>Coffee &amp; donuts</td> <!-- 33-419 -->
+    <td></td> <!-- 33-418 -->
+    <td></td> <!-- 33-422 -->
+    <td></td> <!--  1-135 -->
+    <td></td> <!--  1-136 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>11:00 AM</td>
+    <td> Hacking, etc.</td> <!-- 33-419 -->
+    <td></td> <!-- 33-418 -->
+    <td></td> <!-- 33-422 -->
+    <td></td> <!--  1-135 -->
+    <td></td> <!--  1-136 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>12:00 PM</td>
+    <td> Hacking, etc.</td> <!-- 33-419 -->
+    <td></td> <!-- 33-418 -->
+    <td></td> <!-- 33-422 -->
+    <td>&quot;Role-Playing Games for Fun and Profit: Usability Testing,&quot;
+      Anna Dirks.</td> <!--  1-135 -->
+    <td>&quot;Building applications with the GStreamer system,&quot; Thomas Vander Stichele</td> <!--  1-136 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>1:00 PM</td>
+    <td colspan=5 align=center bgcolor="#fc87de">Lunch</td>
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>2:00 PM</td>
+    <td> Hacking, etc.</td> <!-- 33-419 -->
+    <td rowspan=2>&quot;The File Selector,&quot; Havoc Pennington</td> <!-- 33-418 -->
+    <td></td> <!-- 33-422 -->
+    <td></td> <!--  1-135 -->
+    <td rowspan=2>&quot;The new gnome.org web site,&quot; Jeff Waugh, Tim Ney, et al.</td> <!--  1-136 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>3:00 PM</td>
+    <td> Hacking, etc.</td> <!-- 33-419 -->
+    <td></td> <!-- 33-422 -->
+    <td></td> <!--  1-135 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>4:00 PM</td>
+    <td> Hacking, etc.</td> <!-- 33-419 -->
+    <td rowspan=2>&quot;Building a GNOME Media Player,&quot; Thomas Vander Stichele</td> <!-- 33-418 -->
+    <td rowspan=2>&quot;GNOME Development and Release Process,&quot; Michael Meeks.</td> <!-- 33-422 -->
+    <td></td> <!--  1-135 -->
+    <td></td> <!--  1-136 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>5:00 PM</td>
+    <td> Hacking, etc.</td> <!-- 33-419 -->
+    <td></td> <!--  1-135 -->
+    <td></td> <!--  1-136 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>6:00 PM</td>
+    <td> Hacking, etc.</td> <!-- 33-419 -->
+    <td></td> <!-- 33-418 -->
+    <td></td> <!-- 33-422 -->
+    <td></td> <!--  1-135 -->
+    <td></td> <!--  1-136 -->
+</table>
+
+<h2>Saturday, July 20th</h2>
+<table cellpadding=5 cols=3 width="700" text="#ffffff" align=center>
+<tr bgcolor="#000000" align=center>
+    <td width="16%" bgcolor="white"><font color="#ffffff"></font></td>
+    <td width="16%"><font color="#ffffff">Room 33-419</font></td>
+    <td width="16%"><font color="#ffffff">Room 33-418</font></td>
+    <td width="16%"><font color="#ffffff">Room 33-422</font></td>
+    <td width="16%"><font color="#ffffff">Room 1-135</font></td>
+    <td width="16%"><font color="#ffffff">Room 1-136</font></td>
+</tr>
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>10:00 AM</td>
+    <td>Coffee &amp; donuts</td> <!-- 33-419 -->
+    <td></td> <!-- 33-418 -->
+    <td></td> <!-- 33-422 -->
+    <td></td> <!--  1-135 -->
+    <td></td> <!--  1-136 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>11:00 AM</td>
+    <td> Hacking, etc.</td> <!-- 33-419 -->
+    <td></td> <!-- 33-418 -->
+    <td></td> <!-- 33-422 -->
+    <td></td> <!--  1-135 -->
+    <td></td> <!--  1-136 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>12:00 PM</td>
+    <td> Hacking, etc.</td> <!-- 33-419 -->
+    <td>&quot;ABI Compatibility and ABICheck,&quot; Karl Runge</td> <!-- 33-418 -->
+    <td></td> <!-- 33-422 -->
+    <td>&quot;Demo of the X.Org Media Application Server,&quot; Leon Shiman</td> <!--  1-135 -->
+    <td></td> <!--  1-136 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>1:00 PM</td>
+    <td colspan=5 align=center bgcolor="#fc87de">Lunch</td>
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>2:00 PM</td>
+    <td> Hacking, etc.</td> <!-- 33-419 -->
+    <td rowspan=2>&quot;Making Accessibility work for users,&quot; Bill Haneman.</td> <!-- 33-418 -->
+    <td></td> <!-- 33-422 -->
+    <td rowspan=2>&quot;GU4DEC and GNOME event planning," Tim Ney et al.</td> <!--  1-135 -->
+    <td></td> <!--  1-136 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>3:00 PM</td>
+    <td> Hacking, etc.</td> <!-- 33-419 -->
+    <td></td> <!-- 33-422 -->
+    <td></td> <!--  1-136 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>4:00 PM</td>
+    <td> Hacking, etc.</td> <!-- 33-419 -->
+    <td></td> <!-- 33-418 -->
+    <td></td> <!-- 33-422 -->
+    <td></td> <!--  1-135 -->
+    <td></td> <!--  1-136 -->
+<tr bgcolor="#c8d4e2">
+    <td bgcolor="#acaba9" align=right>5:00 PM</td>
+    <td>Closing address, Havoc Pennington.</td> <!-- 33-419 -->
+    <td colspan=4 bgcolor="#ffffff"></td> <!-- 33-418 -->
+</table>
+    	</td></tr>
+        </table>
+      </td></tr>
+  </table>
+</center>
+
+</body>
+</html>
+ 
