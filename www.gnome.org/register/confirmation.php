f66f2e6ccc18f26e18e44aea02a26cb5533a8dcc
Steve Fox <drfickle@src.gnome.org>
2002-11-06 05:11:51 +0000

    undo test

--- www.gnome.org/register/confirmation.php
+++ www.gnome.org/register/confirmation.php
@@ -129,7 +129,7 @@ if ($show_confirm) {
 		. "An admin will need to approve the registration at:\n\n"
 		. "http://".$sys_default_domain."/admin/\n\n";
 	mail ("webmaster@".$GLOBALS['sys_default_domain'], "New project added to Software Map",
-		$message, "From: drfickle@gnome.org");
+		$message, "From: webmaster@".$GLOBALS['sys_default_domain']);
 
 	$HTML->header(array('title'=>'Registration Complete'));
 	
