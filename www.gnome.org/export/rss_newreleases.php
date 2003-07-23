9da05b620cd371d1a06e3bf2428fe8c12c59aeec
Steve Fox <drfickle@src.gnome.org>
2003-07-23 19:01:44 +0000

    Fix date format to be RFC 822 compliant. Add date entry for time release was made.

--- www.gnome.org/export/rss_newreleases.php
+++ www.gnome.org/export/rss_newreleases.php
@@ -27,8 +27,8 @@ $res = db_query ($query);
 
 // ## one time output
 print " <channel>\n";
-print "  <copyright>Copyright 2002 Free Software Foundation</copyright>\n";
-print "  <pubDate>".gmdate('D, d M Y g:i:s',time())." GMT</pubDate>\n";
+print "  <copyright>Copyright 2003 GNOME Foundation</copyright>\n";
+print "  <pubDate>".gmdate('r',time())." GMT</pubDate>\n";
 print "  <description>GNOME New Releases</description>\n";
 print "  <link>http://$GLOBALS[sys_default_domain]</link>\n";
 print "  <title>GNOME New Releases</title>\n";
@@ -43,6 +43,7 @@ while ($row = db_fetch_array($res)) {
 		print "   <link>http://".$GLOBALS[sys_default_domain]."/softwaremap/projects/"
 			. $row[unix_group_name]."</link>\n";
 		print "   <description>".rss_description($row[short_description])."</description>\n";
+		print "   <pubDate>".gmdate('r',$row['release_date'])." GMT</pubDate>\n";
 		print "  </item>\n";
 		$outputtotal++;
 	}
