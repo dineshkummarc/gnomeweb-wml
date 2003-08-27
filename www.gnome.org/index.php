e196db3cc37cd7572a8d128f0c3c16d883cf2e68
Jeff Waugh <jdub@src.gnome.org>
2003-08-27 00:15:39 +0000

    SOFTWARE PATENTS SMELL LIKE MONKEY BALLS

---
+++ www.gnome.org/index.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+  <title>Protest!</title>
+
+  <meta http-equiv="Refresh" content="20; URL=http://swpat.ffii.org/">
+
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+  <style type="text/css"><!--
+body {color:#000; background:#fff; margin-left:16%; margin-right:16%;}
+h1 {text-align:center;}
+--></style>
+</head>
+<body>
+
+<div align="center">
+<h1>Page closed</h1>
+
+<h3>Page ferm&eacute;e</h3>
+
+<h3>Seite gesperrt</h3>
+ </div>
+
+<p> <font size="+1"> <b> This page is temporarily closed in protest against
+software patents.  Websites may soon be closed down regularly due to software
+patents.  Software patents can get you prosecuted for publishing texts you
+wrote yourself! </b></font> </p>
+
+<p>You will be redirected to <a href="http://swpat.ffii.org/">swpat.ffii.org</a>
+in 20 seconds for more information.  To enter this site, click <a
+ href="index.html">here</a>.</p>
+
+<hr>
+<p>Diese Seite wurde zum Protest gegen Logikpatente vor&uuml;bergehend gesperrt.
+ Bald k&ouml;nnten Webseiten regelm&auml;&szlig;ig wegen Logikpatenten gesperrt
+werden.  Logikpatente k&ouml;nnen dazu f&uuml;hren, dass Sie wegen Ver&ouml;ffentlichung
+selbst entwickelter Texte gerichtlich belangt werden.</p>
+
+<p>Sie werden in 20 Sekunden an <a href="http://swpat.ffii.org/">swpat.ffii.org</a>
+weiter geleitet werden und dort mehr &uuml;ber Logikpatente erfahren.  Um
+zu Ihrem Ziel zu gelangen, klicken Sie bitte <a href="index.html">hier</a>.
+ </p>
+<hr>
+<p>Cette page a &eacute;t&eacute; ferm&eacute;e temporairement en signe de
+protestation contre les brevets logiciels.  Bient&ocirc;t, les pages web
+pourront &ecirc;tre ferm&eacute;es r&eacute;guli&egrave;rement &agrave;
+cause de brevets logiciels.  Les brevets logiciels peuvent faire en sorte
+que vous pourriez &ecirc;tre puni a cause de textes que vous avez cr&eacute;&eacute;
+ind&eacute;pendemment.</p>
+
+<p>Vous serez redirig&eacute; vers <a href="http://swpat.ffii.org/">swpat.ffii.org</a>
+dans 20 secondes.  Pour arriver a v&ocirc;tre d&eacute;stination, veuillez
+cliquer <a href="index.html">ici</a>.  </p>
+</body>
+</html>
+
