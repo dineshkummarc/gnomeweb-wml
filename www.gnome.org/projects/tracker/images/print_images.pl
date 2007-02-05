73b5c7ccc51723c884a72a1efa4f8a6a2d189229
Michele Mattioni <mmattioni@src.gnome.org>
2007-02-05 14:25:18 +0000

    Inserted some screenshots : tracker nautilus integration e deskbar integration
    svn path=/trunk/; revision=4979

---
+++ www.gnome.org/projects/tracker/images/print_images.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Cwd;
+
+my $dir = getcwd;
+
+opendir(DIRHANDLE, $dir);
+
+my @files_names = readdir(DIRHANDLE);
+
+@files_names = sort(@files_names);
+
+foreach (@files_names) {
+ 
+	next if (substr($_,0,1) eq '.');
+	if( $_ =~ m/\w+.png/ ) {
+		print " $_ \\ \n";
+	}
+	
+}
+closedir(DIRHANDLE);
