da59b2649e86180b5bd6af8e45d558aed30751f7
Daniel G. Siegel <dgsiegel@src.gnome.org>
2007-09-29 19:15:22 +0000

    new website for cheese
    svn path=/trunk/; revision=5565

--- www.gnome.org/projects/cheese/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-FILES="screenshots.tmp index.tmp"
-FOOTER="footer.tmp"
-
-for i in $FILES; do
-  cp $i $(echo $i | sed "s/tmp/html/")
-  cat $FOOTER >> $(echo $i | sed "s/tmp/html/")
-done
